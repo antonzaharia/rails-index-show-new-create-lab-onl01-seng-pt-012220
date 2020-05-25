e6281c47e097685aacf34563401c1fae498bad68
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-05-25 18:34:11 +0000

    Automatically backed up by Learn

--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe CouponsController, type: :controller do
 
   describe "GET #create" do
     it "returns http success" do
-      get :create
+      get :new
       expect(response).to have_http_status(:success)
     end
   end
